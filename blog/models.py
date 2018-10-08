563d7f1a427592566dfc7bb43ff76a2ee8e2b825
Dave Weizenegger <dave.weizenegger@gmail.com>
2018-10-07 19:51:57 -0700

    added some bootstrap

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Blog(models.Model):
+    title = models.CharField(max_length=255)
+    create_date = models.DateTimeField()
+    post = models.TextField()
+    image = models.ImageField(upload_to='images/')
